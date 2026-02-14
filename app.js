a02a8655fecac6b90113475e686935345c60bca6
Shubham Chakraborty <chakrabortyshubham00@gmail.com>
2026-02-14 06:03:30 +0000

    Include reusable template

--- app.js
+++ app.js
@@ -2,11 +2,18 @@ const Express = require("express");
 const path = require("node:path");
 const app = Express();
 
+const links = [
+    { href: "/", text: "Home" },
+    { href: "about", text: "About" },
+];
+
+const users = ["Rose", "Cake", "Biff"];
+
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 app.get("/", (_, res) => {
-    res.render("index", { message: "EJS Rocks!" });
+    res.render("index", { message: "EJS Rocks!", links: links, users: users });
 });
 
 app.listen(3000, (err) => {
