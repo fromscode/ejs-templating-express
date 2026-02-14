5c84107bf9a8266d83fea2d0ea596af3ae9863fb
Shubham Chakraborty <chakrabortyshubham00@gmail.com>
2026-02-14 05:34:00 +0000

    Add views

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const Express = require("express");
+const path = require("node:path");
 const app = Express();
 
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
 app.get("/", (_, res) => {
-    res.send("Hello world");
+    res.render("index", { message: "EJS Rocks!" });
 });
 
 app.listen(3000, (err) => {
