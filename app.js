e6635bcea1dd858148b53eb82af27522811e7587
Shubham Chakraborty <chakrabortyshubham00@gmail.com>
2026-02-14 06:16:06 +0000

    Serve static resources: stylesheet

--- app.js
+++ app.js
@@ -7,10 +7,14 @@ const links = [
     { href: "about", text: "About" },
 ];
 
+const assetsPath = path.join(__dirname, "public");
+
 const users = ["Rose", "Cake", "Biff"];
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+
+app.use(Express.static(assetsPath));
 
 app.get("/", (_, res) => {
     res.render("index", { message: "EJS Rocks!", links: links, users: users });
